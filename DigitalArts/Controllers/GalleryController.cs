8d705e16aba94204b5a767ca1dbb579e2a6a2647
Pumzow <32910165+Pumzow@users.noreply.github.com>
2021-07-13 14:23:45 +0300

    Gallery and Builder Extensions
    Small implementations:
    - Gallery Controller/View
    - Application Builder Extensions for Database setup

---
+++ DigitalArts/Controllers/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalArts.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public ActionResult Arts()
+        {
+            return View();
+        }
+    }
+}
---
+++ DigitalArts/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using DigitalArts.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalArts.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder PrepareDatabase(
+            this IApplicationBuilder app)
+        {
+            using var scopedServices = app.ApplicationServices.CreateScope();
+
+            var data = scopedServices.ServiceProvider.GetService<DigitalArtsDbContext>();
+
+            data.Database.Migrate();
+
+            return app;
+        }
+    }
+}
